931072642e68233d47d98f35de6c8b657fde793b
veerlorden <7z95A0dJ73h213>
2021-07-03 19:07:37 +0300

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Вадим Буркин on 03.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
